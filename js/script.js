d4977c08f4df83e30a35140a912787af48150ba9
Guntur67 <gntredits@gmail.com>
2026-09-14 12:56:49 +0700

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
 const promoButton = document.querySelector("#promoButton");
 
 promoButton.addEventListener("click", () => {
-    promoButton.textContent = "Promo: Beli 2 gratis tester!";
+    promoButton.textContent = "Promo: Beli 2 gratis voucher diskon";
     console.log("Promo Kopi Nuasa berhasil di tampilkan. ");
 });
